c8069a2e6deeb4bc5b0537def630c4a86ea7be22
Chanwit Kaewkasi <chanwit@gmail.com>
2023-08-02 12:59:30 +0700

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.3.9"
+}
+
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
+}
+
+output "hello_world" {
+  value = "Hello branch demo, ${var.subject}!"
+}
